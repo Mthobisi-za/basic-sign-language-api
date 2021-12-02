c6501d50cd94c773f4665a850cc92230088fdef8
Mthobisi-za <mthobisimtho333@gmail.com>
2021-12-02 14:20:57 +0200

    sql

--- index.js
+++ index.js
@@ -28,30 +28,29 @@ if (connectionstr) {
     });
 }
 
-app.get('/', async(req, res) => {
-    res.render('index.html')
-})
 
-app.get('/user/:name/pts/:pts', async(req, res) => {
+
+app.get('/newuser/:name', async(req, res) => {
     var name = req.params.name;
-    var pts = req.params.pts
-        //register user
-    await pool.query('update set player(name, player_score) values($1, $2)', [name, 0]);
+    //register user
+    await pool.query('insert into player(name) values($1)', [name]);
     res.json({ name });
 });
 
 app.get('/getuser/:name', async(req, res) => {
     var name = req.params.name;
     var userName = (await pool.query('select * from player where name= $1', [name])).rows;
-    var data = userName[0]
     if (userName.length == 0) {
-        await pool.query('insert into player(name, player_score) values($1, $2)', [name, 0]);
-        var recent = (await pool.query('select * from player where name= $1', [name])).rows;
-        res.json({
-            data: recent[0]
-        })
+        res.json({ err: 'no such username' })
     } else {
-        res.json({ data })
+        var usersData = (await pool.query('select * from player_exercise where player_id = $1', [userName[0].id])).rows;
+        console.log(usersData)
+        if (usersData.length == 0) {
+            res.json({ name: userName[0].name, userStatus: 'New User' });
+        } else {
+            res.json({ name: userName[0].name, data: usersData[0], userStatus: 'Have played before' });
+        };
+
     }
 });
 
@@ -59,14 +58,40 @@ app.post('/submit', async(req, res) => {
     ///submit/:name/level:levelName/score/:score
     var name = req.body.name;
     var level = req.body.levelName;
-    var score = Number(req.body.player_score);
-    await pool.query('update player set player_score = player_score + $1', [score]);
+    var score = Number(req.body.score);
+    console.log(name, level, score);
+    //first check if they have level
+
+    var userData = (await pool.query('select * from player where name= $1', [name])).rows;
+    if (userData.length == 0) {
+        //user does not exist
+        await pool.query('insert into player(name) values($1)', [name]);
+        var id = ((await pool.query('select id from player where name= $1', [name])).rows)[0].id;
+        await pool.query(`insert into player_exercise (player_id,level,player_score) 
+            values($1,$2,$3)`, [id, level, score]);
+
+    } else {
+        var usersData = (await pool.query('select * from player_exercise where player_id = $1', [userData[0].id])).rows;
+        //check if the have level
+        if (usersData.length == 0) {
+            // wecan just insert
+            await pool.query(`insert into player_exercise (player_id,level,player_score) 
+            values($1,$2,$3)`, [userData[0].id, level, score]);
+
+        } else {
+            //we can update
+            await pool.query('update player_exercise set level = $1, score = score + $2', [level, score]);
+
+        };
+    }
+});
+app.get('/board', async(req, res) => {
+    var usersData = (await pool.query('select * from player_exercise')).rows;
+    res.json({
+        data: usersData
+    })
 });
 
-app.get('/getlevel', async(req, res) => {
-    var level = (await pool.query('select * from levels')).rows;
-    res.json({ data: level })
-});
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log('Server started on ' + PORT);
