a4ae657e17052b444d7ec7dc72c2d23a7e130048
Mthobisi-za <mthobisimtho333@gmail.com>
2021-12-02 07:01:33 +0200

    wait

--- tables.sql
+++ tables.sql
@@ -1,6 +1,16 @@
-create table player(id serial primary key, name text not null);
+create table player(id serial primary key, name text not null, player_score int not null);
 
-/*create table exercises(id serial primary key, level_name text not null);*/
+create table levels(id serial primary key, level_name text not null);
 
-create table player_exercise(player_id int, level text not null, player_score int not null,
-foreign key(player_id) references player(id));
+/*
+create table player_exercise(player_id int, level_id, player_score int not null,
+foreign key(player_id) references player(id),
+foreign key(level_id) references levels(id)
+
+); */
+
+insert into levels(level_name) values('Please');
+
+insert into levels(level_name) values('Thank You');
+
+insert into levels(level_name) values('Hello');
